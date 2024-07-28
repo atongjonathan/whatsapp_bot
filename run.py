82df7da9f680745831833739d040775032a679a8
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2024-07-28 16:34:14 +0300

    feat: keep_alive

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import logging
 
 from .app import create_app
-
+from threading import Thread
 
 app = create_app()
 logging.basicConfig(format="SGBot | %(asctime)s | %(levelname)s | %(module)s | %(lineno)s | %(message)s", level=logging.INFO, handlers=[
@@ -10,6 +10,13 @@ logging.basicConfig(format="SGBot | %(asctime)s | %(levelname)s | %(module)s | %
 ])
 logger = logging.getLogger(__name__)
 logging.info("Whatsapp bot started")
-# if __name__ == "__main__":
-#     logging.info("Flask app started")
-#     app.run(host="0.0.0.0", port=8000)
+
+
+def keep_alive():
+    t = Thread(target=app.run(host="0.0.0.0", port=8000))
+    t.start()
+
+
+if __name__ == "__main__":
+    logging.info("Flask app started")
+    keep_alive()

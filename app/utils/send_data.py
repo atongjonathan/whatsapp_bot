da068fa4cd5b087cb4d17514d1eef3da1394e7e2
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2024-07-29 12:38:05 +0300

    feat: start

--- app/utils/send_data.py
+++ app/utils/send_data.py
@@ -9,7 +9,7 @@ import telebot
 ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 PHONE_NUMBER_ID = os.environ.get("PHONE_NUMBER_ID")
 TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
-bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN) 
+bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 VERSION = os.environ.get("VERSION")
 headers = {
     "Content-type": "application/json",
@@ -41,13 +41,6 @@ def mark_as_read(message_id):
         logging.error(e)
         return jsonify({"status": "error", "message": "Failed to mark message as read"}), 500
 
-# def reply_message(message):
-#     message_id = message.get("id")
-#     if message_type == "text":
-#         message_body = message["text"]["body"]
-#         send_message(message_body, number, message_type)
-#     else:
-#         logging.info(message)
 
 
 def call_api(message_body):
@@ -69,118 +62,18 @@ def call_api(message_body):
         return jsonify({"status": "error", "message": "Failed to send message"}), 500
 
 
-def send_message(message):
-    message_type = message.get("type")
+def send_text(chat_id, text, message_id):
     body = {
         "messaging_product": "whatsapp",
         "context": {
-            "message_id": message.get("id")
+            "message_id": message_id
         },
-        "to": message.get("from"),
-        "type": message_type
+        "to": chat_id ,
+        "type": "text",
+        "text": {
+            "preview_url": True,
+            "body": text
+        }
     }
-    if message_type == "text":
-        text = message["text"]["body"]
-        if "-" in text:
-            performer = text.split("-")[0].strip()
-            title = text.split("-")[1].strip()
-            try:
-                url = f"https://api.atongjonathan2.workers.dev/?title={title}&performer={performer}"
-                response = requests.get(url)
-                response.raise_for_status()
-                data = response.json()
-                file_id = data["response"]["document"]["file_id"]
-                file_info = bot.get_file(file_id) 
-                url = 'https://api.telegram.org/file/bot{0}/{1}'.format(TELEGRAM_BOT_TOKEN, file_info.file_path) 
-                body["type"] = "document"
-                body["document"] = {
-                    "link": url,
-                    "caption": f"{performer} = {title}",
-                    "filename":f"{performer} = {title}.mp3"
-                }              
-            except Exception as e:
-
-                body[message_type] = {"preview_url": False, "body": str(e)}
-    elif message_type == "contacts":
-        #         contact_body = {
-        #   "messaging_product": "whatsapp",
-        #   "to": "254708683896",
-        #   "type": "contacts",
-        #   "contacts": [
-        #     {
-        #       "name": {
-        #         "formatted_name": "Jona",
-        #         "first_name": "Jonathan",
-        #         "last_name": "Atong"
-        #       },
-        #       "phones": [
-        #         {
-        #           "phone": "+254708683896",
-        #           "type": "CELL"
-        #         }
-        #       ],
-        #       "emails": [
-        #         {
-        #           "email": "atongjonathan@gmail.com",
-        #           "type": "HOME"
-        #         }
-        #       ],
-        #       "addresses": [
-        #         {
-        #           "street": "Komarock",
-        #           "city": "Nairobi",
-        #           "state": "NRB",
-        #           "zip": "00100",
-        #           "country_code": "254",
-        #           "country": "KE",
-        #           "type": "HOME"
-        #         }
-        #       ],
-        #       "birthday": "2001-11-16",
-        #       "org": {
-        #         "company": "Peri Bloom",
-        #         "department": "Developers",
-        #         "title": "Developer"
-        #       },
-        #       "urls": [
-        #         {
-        #           "url": "https://peri-bloom.com",
-        #           "type": "WORK"
-        #         }
-        #       ]
-        #     }
-        #   ]
-        # }
-        body[message_type] = message["contacts"]
-    elif message_type == "image":
-        body[message_type] = {
-            "id": message["image"]["id"]
-        }
-        caption = message["image"].get("caption")
-        if caption:
-            body[message_type]["caption"] = caption
-    elif message_type == "video":
-        body[message_type] = {
-            "id": message["video"]["id"]
-        }
-        caption = message["video"].get("caption")
-        if caption:
-            body[message_type]["caption"] = caption
-    elif message_type == "audio":
-        body[message_type] = {
-            "id": message["audio"]["id"]
-        }
-    elif message_type == "document":
-        body[message_type] = {
-            "id": message["document"]["id"]
-        }
-        caption = message["document"].get("caption")
-        if caption:
-            body[message_type]["caption"] = caption
-    else:
-        body["text"] = {
-            "body": f"This is a {message_type}"
-        }
-
-    # logging.info(body[message_type])
     call_api(body)
+
--- app/utils/whatsapp_utils.py
+++ app/utils/whatsapp_utils.py
@@ -3,18 +3,38 @@ import datetime
 import pytz
 # from app.services.openai_service import generate_response
 import re
-from .send_data import send_message, mark_as_read
+from .send_data import send_text, mark_as_read
 
+
+help = {
+    # command description used in the "help" command
+    'start': 'Get used to the bot',
+    'help': 'Gives you information about the available commands',
+    'song': 'Search for a song',
+    'artist': 'Search for an artist',
+    'trending': 'Get hits of the week',
+    'snippet': 'Listen to part of the song',
+    'ping': 'Test me'
+}
+commands = [f"/{key}" for (key, value) in help.items()]
+help_text = "The following commands are available: \n\n"
+for key in commands:  # generate help text out of the commands dictionary defined at the top
+    help_text += "/" + key + ": "
+    help_text += commands[key] + "\n"
+help_text += """
+Example usage:
+/artist Burna Boy or /artist only and reply with the name
+/song Closer - Halsey  or /song only and reply with the name
+/trending 10 - Get top 10 trending songs on Billboard Top 100
+/snippet Closer - Halsey : Get 30 second preview of the song
+    """
 
 def convert_time(timestamp):
     utc_time = datetime.datetime.utcfromtimestamp(int(timestamp))
     east_africa_timezone = pytz.timezone('Africa/Nairobi')
-    eat_time = utc_time.replace(tzinfo=pytz.utc).astimezone(east_africa_timezone)
+    eat_time = utc_time.replace(
+        tzinfo=pytz.utc).astimezone(east_africa_timezone)
     return eat_time.strftime('%Y-%m-%d %H:%M:%S')
-
-
-
-
 
 
 def process_text_for_whatsapp(text):
@@ -35,7 +55,6 @@ def process_text_for_whatsapp(text):
     return whatsapp_style_text
 
 
-
 def process_whatsapp_message(body):
     number = body["entry"][0]["changes"][0]["value"]["contacts"][0]["wa_id"]
     name = body["entry"][0]["changes"][0]["value"]["contacts"][0]["profile"]["name"]
@@ -44,19 +63,17 @@ def process_whatsapp_message(body):
     message_type = message.get("type")
     timestamp = message.get("timestamp")
     message_id = message.get("id")
-    logging.info(f"Incoming {message_type} message from {name}, phone number: {number} at {convert_time(timestamp)} id: {message_id}")
+    logging.info(
+        f"Incoming {message_type} message from {name}, phone number: {number} at {convert_time(timestamp)} id: {message_id}")
     mark_as_read(message_id)
-    send_message(message)
-
-
-    # TODO: implement custom function here
-
-
-    # OpenAI Integration
-    # response = generate_response(message_body, wa_id, name)
-    # response = process_text_for_whatsapp(response)
-
-
+    message_type = message.get("type")
+    message_id = message.get("id")
+    chat_id = message.get("from")
+    if message_type == "text":
+        text = message["text"]["body"].strip()
+        text_list = [word for word in text]
+        if text_list[0] in commands:
+            send_text(chat_id, help_text, message_id)
 
 
 

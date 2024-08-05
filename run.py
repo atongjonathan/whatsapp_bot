966bf4e4aeb7283f1bfa77232fa50a8ab3619a32
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2024-08-05 22:45:36 +0300

    comment unused

--- run.py
+++ run.py
@@ -1,13 +1,7 @@
-from logging import basicConfig, INFO, StreamHandler, FileHandler
-
 from . app import create_app
 from threading import Thread
 
 app = create_app()
-basicConfig(format="SGWABot | %(asctime)s | %(levelname)s | %(module)s | %(lineno)s | %(message)s", level=INFO, handlers=[
-    StreamHandler(),
-    FileHandler("logs.txt")
-])
 
 
 def keep_alive():
